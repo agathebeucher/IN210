afb6fb9854cfa03862329c87118fbf2d2f243b9d
mathias.gilbert <mathias.gilbert42@gmail.com>
2024-02-16 10:54:35 +0100

    Code mis en forme

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdarg.h>
@@ -12,45 +11,52 @@
 /* A test case that does nothing and succeeds. */
 
 // Déclaration du mock
-void recupAge(int* age){
-    *age=(int)mock();
+void recupAge(int *age)
+{
+    *age = (int)mock();
 }
 
-static int setup(void **state) {
-    (void) state;
+static int setup(void **state)
+{
+    (void)state;
     printf("setUp");
- return 0;
+    return 0;
 }
-static int teardown(void **state) {
-    (void) state;
+static int teardown(void **state)
+{
+    (void)state;
     printf("tearDown");
-     return 0;
+    return 0;
 }
-static void null_test_success(void **state) {
-    (void) state;
+static void null_test_success(void **state)
+{
+    (void)state;
 }
-static void test_tourist_float(void **state){
-    (void) state;
-    will_return(recupAge,12);
-    float prix=computePrice(20.0,0);
+static void test_tourist_float(void **state)
+{
+    (void)state;
+    will_return(recupAge, 12);
+    float prix = computePrice(20.0, 0);
     // Utilisation du mock
-    assert_float_equal(1.5,prix,0.001);
+    assert_float_equal(1.5, prix, 0.001);
 }
 
-static void test_tourist_int(void **state){
-    (void) state;
-    will_return(recupAge,12);
-    float prix=computePrice(20.0,0);
-    assert_true(2.0==prix);
+static void test_tourist_int(void **state)
+{
+    (void)state;
+    will_return(recupAge, 12);
+    float prix = computePrice(20.0, 0);
+    assert_true(2.0 == prix);
 }
 
-int main(void) {
+int main(void)
+{
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
         cmocka_unit_test(test_tourist_float),
         cmocka_unit_test(test_tourist_int),
     };
-//    cmocka_set_message_output(CM_OUTPUT_XML);
-    return cmocka_run_group_tests_name("toto",tests, setup, teardown);
-    //prend le tableau qui s'apelle tests défini précédemment
+    //    cmocka_set_message_output(CM_OUTPUT_XML);
+    return cmocka_run_group_tests_name("toto", tests, setup, teardown);
+    // prend le tableau qui s'apelle tests défini précédemment
 }
